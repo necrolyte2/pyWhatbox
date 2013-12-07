318a8e66fb84bc49ee60e1ff4b53cb4ba04ec729
Tyghe Vallard <vallardt@gmail.com>
2013-12-07 09:54:59 -0500

    Allows user to specify hashs to download. Also should delete download when finished. Still working on that

--- wb_sync_completed.py
+++ wb_sync_completed.py
@@ -7,31 +7,36 @@ import os
 import re
 import subprocess
 import shlex
+import sys
 
 # Default url path to finished downloads
 # on the host
 PRIVATEFINISHED='/private/finished'
 
-def main():
-    args = parse_args()
-    sync_completed( args.username, args.password, args.host, args.xmlpath, args.destdir, args.finishedpath )
+# Whatbox instance
+what = None
 
-def sync_completed( username, password, host, xmlpath, syncdir, finishedpath ):
-    w = WhatboxXMLRPC(
-            host,
-            username,
-            password,
-            xmlpath
-    )
-    
+def main():
+    global what
+    args = parse_args()
+    what = WhatboxXMLRPC( args.host, args.username, args.password, args.xmlpath )
+
+    alldls = what.get_all_files()
+    if args.hashs:
+        hashs = {h:alldls[h] for h in args.hashs}
+    else:
+        hashs = alldls
+
+    sync_completed( args.username, args.password, args.host, args.xmlpath, args.destdir, args.finishedpath, hashs )
+
+def sync_completed( username, password, host, xmlpath, syncdir, finishedpath, torrents ):
     if finishedpath != '/':
         finishedpath += '/'
     basesyncurl = 'https://{}{}'.format(host, finishedpath)
 
-    torrents = w.get_all_files()
-    for hash, info in torrents.items():
+    for dlhash, info in torrents.items():
         if info['complete'] == 1:
-            sync_torrent( info, basesyncurl, syncdir, username, password )
+            sync_torrent( (dlhash,info), basesyncurl, syncdir, username, password )
 
 def should_download( download_path ):
     ''' Filter downloads and check for their existence '''
@@ -44,7 +49,8 @@ def should_download( download_path ):
 
     return True
 
-def sync_torrent( torrent_info, baseurl, syncdir, username, password, conns=16 ):
+def sync_torrent( torrent, baseurl, syncdir, username, password, conns=16 ):
+    dlhash, torrent_info = torrent
     if baseurl[-1] != '/':
         baseurl = baseurl + '/'
         
@@ -58,6 +64,7 @@ def sync_torrent( torrent_info, baseurl, syncdir, username, password, conns=16 )
             raise e
 
     downloads = []
+    failed = False
     # Loop through all files in the torrent
     for f in torrent_info['files']:
         # Split the file name up by each /
@@ -72,13 +79,22 @@ def sync_torrent( torrent_info, baseurl, syncdir, username, password, conns=16 )
                 '--http-user {username} --http-pass {password} --check-certificate=false "{url}"'.format(
                     conns=conns, username=username, password=password, url=url
             )
-            p = subprocess.Popen( shlex.split(cmd), stdout=subprocess.PIPE, stderr=subprocess.STDOUT, cwd=dstdir )
+            try:
+                p = subprocess.Popen( shlex.split(cmd), stdout=subprocess.PIPE, stderr=subprocess.STDOUT, cwd=dstdir )
+            except OSError as e:
+                print "Please install aria2c"
+                sys.exit( -1 )
             for line in iter(p.stdout.readline, b''):
                 print line.rstrip()
+            # Wait for the process to finish
+            p.wait()
             if p.returncode != 0:
                 print "{} failed".format(cmd)
+                print "return code was {}".format(p.returncode)
+                failed = True
     # Should like check to make sure it is actually downloaded now
-    # Then delete the source torrent and files
+    if not failed:
+        what.delete_download( dlhash )
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -96,13 +112,18 @@ def parse_args():
         dest='destdir',
         help='Directory to sync downloads to'
     )
-
     parser.add_argument(
         '-p',
         default='/xmlrpc',
         dest='xmlpath'
     )
-
+    parser.add_argument(
+        '--hash',
+        dest='hashs',
+        nargs='*',
+        default=[],
+        help='Hashs to download or do them all if not specified'
+    )
     parser.add_argument(
         '--fp',
         default=PRIVATEFINISHED,
